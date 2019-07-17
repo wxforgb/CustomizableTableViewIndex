c4cb12284a78ee3f477eb304434be8d7da2625ec
王 きん <oh_kin@cyberagent.co.jp>
2019-07-17 19:04:15 +0900

    Initial Commit

---
+++ CustomizableTableViewIndex/CustomizableTableViewIndex.h
@@ -0,0 +1,18 @@
+//
+//  CustomizableTableViewIndex.h
+//  CustomizableTableViewIndex
+//
+//  Created by 王 きん on 2019/07/17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomizableTableViewIndex.
+FOUNDATION_EXPORT double CustomizableTableViewIndexVersionNumber;
+
+//! Project version string for CustomizableTableViewIndex.
+FOUNDATION_EXPORT const unsigned char CustomizableTableViewIndexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomizableTableViewIndex/PublicHeader.h>
+
+
