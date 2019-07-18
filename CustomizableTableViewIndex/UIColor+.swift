6388bd367a7b3262ab3cf8968080778f79e15253
王 きん <oh_kin@cyberagent.co.jp>
2019-07-18 11:41:10 +0900

    test

---
+++ CustomizableTableViewIndex/UIColor+.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+.swift
+//  CustomizableTableViewIndex
+//
+//  Created by 王 きん on 2019/07/17.
+//
+
+import Foundation
+
+// publicもしくはopenにしておかないと、アプリ側（別モジュール）から利用できない
+public extension UIColor {
+    static var test: UIColor = UIColor.red
+}
---
+++ Demo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by 王 きん on 2019/07/17.
+//
+
+import UIKit
+import CustomizableTableViewIndex
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .test
+    }
+
+
+}
+
